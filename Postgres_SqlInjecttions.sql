811854dce2c06c73b5f281d1b1f3122e78166e0b
Mahmood Azadi <90643392+libertyir@users.noreply.github.com>
2021-09-18 17:33:32 +0430

    Update Postgres_SqlInjecttions.sql

--- Postgres_SqlInjecttions.sql
+++ Postgres_SqlInjecttions.sql
@@ -16,17 +16,18 @@ Select COALESCE(null,'0') ---result if (username == null) '0' else "text of user
 --******
 -- multiple string join
 create table table1(txt text);
-insert table1 values ('a');
-insert table1 values ('b');
-insert table1 values ('c');
+insert into table1 values ('a');
+insert into table1 values ('b');
+insert into table1 values ('c');
 Select string_agg(txt,',') from Table1 -- result a,b,c
+
 
 --database  staructs
 --*********************************
 --return database list
 
 select datname from pg_database
-select string_agg(cast(COALESCE(datname,'')  as varchar) , ',') from pg_database --result all database name in separated string
+select string_agg(datname,',') from pg_database --result all database name in separated string
 
 --*****
 
