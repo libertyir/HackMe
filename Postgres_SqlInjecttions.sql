1c2c242b094a808019e74ebae38fa9f554e9e9e4
Mahmood Azadi <90643392+libertyir@users.noreply.github.com>
2021-09-18 17:31:00 +0430

    Update Postgres_SqlInjecttions.sql

--- Postgres_SqlInjecttions.sql
+++ Postgres_SqlInjecttions.sql
@@ -1,14 +1,17 @@
-Postgres Sql Injections 
+--Postgres Sandbox Admin test
+--https://www.katacoda.com/enterprisedb/scenarios/pgadmin-sandbox
+
+--Postgres Sql Injections 
 
 
 --String Operation 
-/***********************************/
+--***********************************
 --String join 
 Select 'A'  || 'B' --Result AB
 
 --Check Null String or first not null string
 --
-Select COALESCE(usename,'0')  ---result if (username == null) '0' else "text of username"
+Select COALESCE(null,'0') ---result if (username == null) '0' else "text of username"
 
 --******
 -- multiple string join
@@ -18,5 +21,17 @@ insert table1 values ('b');
 insert table1 values ('c');
 Select string_agg(txt,',') from Table1 -- result a,b,c
 
---*******
+--database  staructs
+--*********************************
+--return database list
+
+select datname from pg_database
+select string_agg(cast(COALESCE(datname,'')  as varchar) , ',') from pg_database --result all database name in separated string
+
+--*****
+
+
+
+
+
 
