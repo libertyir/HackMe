24532fff21f6aca50d809433aad8146d6a919cac
Mahmood Azadi <90643392+libertyir@users.noreply.github.com>
2021-09-18 17:43:52 +0430

    Update Postgres_SqlInjecttions.sql

--- Postgres_SqlInjecttions.sql
+++ Postgres_SqlInjecttions.sql
@@ -22,15 +22,28 @@ insert into table1 values ('c');
 Select string_agg(txt,',') from Table1 -- result a,b,c
 
 
---database  staructs
+--database  structure
 --*********************************
 --return database list
 
 select datname from pg_database
 select string_agg(datname,',') from pg_database --result all database name in separated string
 
---*****
+--return tables 
+select table_name from information_schema.tables
+select string_agg(table_name, ',') from information_schema.tables
 
+--return columns 
+select column_name from information_schema.columns where table_name = 'tablename'
+select string_agg(column_name, ',') from information_schema.columns where table_name = 'tablename'
+
+--return tables and fields
+select (table_name || ' : ' || (select string_agg(column_name, ',') from information_schema.columns _c  where _c.table_name = _t.table_name)) as txt1 from information_schema.tables _t
+--sapmle result
+------------------------------------
+--| table1 : field1,field2,field3  |
+--| table2 : field1                |
+------------------------------------
 
 
 
